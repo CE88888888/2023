d7da668a99dfbf7cc5faa68b82f8a222779d6189
CE88888888 <celbersen@hotmail.com>
2023-12-15 16:52:32 +0100

    with cache

--- day14/day14.ts
+++ day14/day14.ts
@@ -9,40 +9,41 @@ console.log(`Part 2 example: ${solve2(example)}`);
 const t0 = performance.now();
 console.log(`Part 2 solution: ${solve2(input)}`);
 const t1 = performance.now();
-console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+console.log(`Call to Solve 2 took ${t1 - t0} milliseconds.`);
+
+type Result = {
+  grid: string;
+  weight: number;
+};
 
 function solve1(lines: string[], sum = 0) {
   let grid = lines.map((x) => [...x]);
-  let x = tiltN(grid);
-  sum = countweight(x);
-  return sum;
+  tiltN(grid);
+  return countweight(grid);
 }
 
 function solve2(lines: string[], weight = 0) {
-  let grid = lines.map((x) => [...x]);
-  let weightresult = new Map<string, number>();
-  let gridstate = new Map<string, string[][]>();
+  const grid = lines.map((x) => [...x]);
+  const l = grid.length;
+  let flat = grid.flat(2).toString();
+
+  let resultcache = new Map<string, Result>();
   let ring = [];
   let ringnotfound = true;
-  for (let i = 0; i < 400; i++) {
-    let flat = grid.flat(2).toString();
-    let x = [];
-    let cachedgrid = gridstate.get(flat);
-    if (cachedgrid !== undefined) {
-      grid = gridstate.get(flat);
-    } else {
-      x = cycle(grid);
-      var copyarr = grid.map((arr) => arr.slice());
-      gridstate.set(flat, copyarr);
-   }
 
-    let cacheresult = weightresult.get(flat);
-    if (cacheresult !== undefined) {
-      weight = cacheresult;
+  for (let i = 0; i < 400; i++) {
+    const cachedresult = resultcache.get(flat);
+    if (cachedresult !== undefined) {
+      flat = resultcache.get(flat).grid;
+      weight = resultcache.get(flat).weight;
     } else {
-      let s = countweight(x);
-      weight = s;
-      weightresult.set(flat, weight);
+      const flatold = flat;
+      flat = cycleFlat(flat, l);
+      const r = {
+        grid: flat,
+        weight: countweightFlat(flatold, l),
+      };
+      resultcache.set(flatold, r);
     }
 
     if (i >= 200) {
@@ -57,29 +58,41 @@ function solve2(lines: string[], weight = 0) {
       }
     }
   }
+
   const ringlength = 400 % ring.length;
   const offset = ring.indexOf(weight) - ringlength;
   const result = ((1000000000 % ring.length) + offset) % ring.length;
   return ring[result];
 }
 
-function cycle(grid: string[][]) {
+function cycleFlat(flatgrid: string, l: number) {
+  let grid = rebuildGrid(l, flatgrid);
   grid = tiltN(grid);
   grid = tiltW(grid);
   grid = tiltS(grid);
   grid = tiltE(grid);
+  return grid.flat(2).toString();
+}
+
+function countweightFlat(flatgrid: string, l: number) {
+  let grid = rebuildGrid(l, flatgrid);
+  return countweight(grid);
+}
+
+function rebuildGrid(l: number, flatgrid: string) {
+  let grid: string[][] = [];
+  for (let i = 0; i < l; i = i + 1) {
+    grid.push(flatgrid.slice(i * 2 * l, i * 2 * l + 2 * l - 1).split(","));
+  }
   return grid;
 }
 
 function countweight(grid: string[][]) {
-  const maxy = grid.length;
-  const maxx = grid[0].length;
   let sum = 0;
-
-  for (let y = 0; y < maxy; y++) {
-    for (let x = 0; x < maxx; x++) {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid.length; x++) {
       if (grid[y][x] === "O") {
-        sum = sum + maxy - y;
+        sum = sum + grid.length - y;
       }
     }
   }
@@ -105,17 +118,14 @@ function tiltSmart(row: string[]) {
 }
 
 function tiltN(grid: string[][]) {
-  const maxy = grid.length;
-  const maxx = grid[0].length;
-
-  for (let x = 0; x < maxx; x++) {
+  for (let x = 0; x < grid.length; x++) {
     let col2row = [];
-    for (let y = maxy - 1; y > -1; y--) {
+    for (let y = grid.length - 1; y > -1; y--) {
       col2row.push(grid[y][x]);
     }
     let n = tiltSmart(col2row);
     n = n.reverse();
-    for (let y = 0; y < maxy; y++) {
+    for (let y = 0; y < grid.length; y++) {
       grid[y][x] = n[y];
     }
   }
@@ -123,16 +133,13 @@ function tiltN(grid: string[][]) {
 }
 
 function tiltS(grid: string[][]) {
-  const maxy = grid.length;
-  const maxx = grid[0].length;
-
-  for (let x = 0; x < maxx; x++) {
+  for (let x = 0; x < grid.length; x++) {
     let col2row = [];
-    for (let y = 0; y < maxy; y++) {
+    for (let y = 0; y < grid.length; y++) {
       col2row.push(grid[y][x]);
     }
     let n = tiltSmart(col2row);
-    for (let y = 0; y < maxy; y++) {
+    for (let y = 0; y < grid.length; y++) {
       grid[y][x] = n[y];
     }
   }
@@ -140,10 +147,7 @@ function tiltS(grid: string[][]) {
 }
 
 function tiltE(grid: string[][]) {
-  const maxy = grid.length;
-  const maxx = grid[0].length;
-
-  for (let y = 0; y < maxy; y++) {
+  for (let y = 0; y < grid.length; y++) {
     let n = tiltSmart(grid[y]);
     grid[y] = n;
   }
@@ -151,10 +155,7 @@ function tiltE(grid: string[][]) {
 }
 
 function tiltW(grid: string[][]) {
-  const maxy = grid.length;
-  const maxx = grid[0].length;
-
-  for (let y = 0; y < maxy; y++) {
+  for (let y = 0; y < grid.length; y++) {
     let n = tiltSmart(grid[y].reverse());
     n = n.reverse();
     grid[y] = n;
