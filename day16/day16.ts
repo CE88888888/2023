7595d943e28bb27a4cff34382d70c01baf0ab09e
CE88888888 <celbersen@hotmail.com>
2023-12-16 13:22:55 +0100

    day 16

--- day16/day16.ts
+++ day16/day16.ts
@@ -9,148 +9,143 @@ console.log(`Part 2 example: ${solve2(example)}`);
 console.log(`Part 2 solution: ${solve2(input)}`);
 
 type Point = {
-  v: string;
   x: number;
   y: number;
   dir: Dir;
 };
+type Dir = "L" | "R" | "U" | "D";
 
-type Dir = "left" | "right" | "up" | "down";
-
-function solve1(lines: string[], sum = 0) {
+function solve1(lines: string[]) {
   let points: Point[] = [];
-  let gridraw = [];
-  lines.forEach((l) => {
-    gridraw.push([...l]);
-  });
+  let grid = [];
+  lines.forEach((l) => grid.push([...l]));
+  points.push({ x: 0, y: 0, dir: "R" });
 
-  const s: Point = {
-    v: gridraw[0][0],
-    x: 0,
-    y: 0,
-    dir: "right",
-  };
-  points.push(s);
+  return calcEnergised(points, grid);
+}
 
-  let ene = new Map<string, Point>();
+function solve2(lines: string[], max = 0) {
+  let grid = [];
+  lines.forEach((l) => grid.push([...l]));
+  let bound = grid.length - 1;
+
+  for (let i = 0; i < grid[0].length; i++) {
+    const s: Point = { x: i, y: 0, dir: "D" };
+    max = Math.max(max, calcEnergised([s], grid));
+  }
+
+  for (let i = 0; i < grid[0].length; i++) {
+    const s: Point = { x: i, y: bound, dir: "U" };
+    max = Math.max(max, calcEnergised([s], grid));
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    const s: Point = { x: bound, y: i, dir: "L" };
+    max = Math.max(max, calcEnergised([s], grid));
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    const s: Point = { x: 0, y: i, dir: "U" };
+    max = Math.max(max, calcEnergised([s], grid));
+  }
+
+  return max;
+}
+
+function calcEnergised(points: Point[], grid: any[]) {
+  let energised = new Set<string>();
+  let visited = new Set<string>();
 
   for (let i = 0; i < points.length; i++) {
     const p = points[i];
-    ene.set(p.x.toString() + "," + p.y.toString(), p);
+    energised.add(p.x.toString() + "," + p.y.toString());
+    visited.add(hash(p));
+    let c = grid[p.y][p.x];
 
-    if (i % 1000 === 0) {
-      console.log(i, ene.size);
-    }
     let np = { ...p };
     let next = [];
 
-    if (p.v === ".") {
+    if (c === ".") {
       next.push(np);
     }
-    if (p.v === "/") {
-      np.dir = rotate(p, "/");
+    if (c === "/") {
+      np.dir = rotate(p.dir, "/");
       next.push(np);
     }
-    if (p.v === "\\") {
-      np.dir = rotate(p, "\\");
+    if (c === "\\") {
+      np.dir = rotate(p.dir, "\\");
       next.push(np);
     }
 
-    if (p.v === "-") {
-      if (p.dir === "left" || p.dir === "right") {
+    if (c === "-") {
+      if (p.dir === "L" || p.dir === "R") {
         next.push(np);
       } else {
         let np1 = { ...p };
         let np2 = { ...p };
-        np1.dir = "left";
-        np2.dir = "right";
+        np1.dir = "L";
+        np2.dir = "R";
         next.push(np1);
         next.push(np2);
       }
     }
 
-    if (p.v === "|") {
-      if (p.dir === "up" || p.dir === "down") {
+    if (c === "|") {
+      if (p.dir === "U" || p.dir === "D") {
         next.push(np);
       } else {
         let np1 = { ...p };
         let np2 = { ...p };
-        np1.dir = "up";
-        np2.dir = "down";
+        np1.dir = "U";
+        np2.dir = "D";
         next.push(np1);
         next.push(np2);
       }
     }
-
 
     next.forEach((x) => {
       x = move(x);
-      if (!outOfBounds(x, gridraw)) {
-        x.v = gridraw[x.y][x.x];
-        if (
-          points.filter((i) => x.x === i.x && x.y === i.y && x.dir === i.dir)
-            .length < 1
-        ) {
-          points.push(x);
-        }
+      if (!outOfBounds(x, grid) && !visited.has(hash(x))) {
+        points.push(x);
       }
     });
   }
 
-  return ene.size; //6774, 7107 too low
+  return energised.size;
 }
 
-function rotate(p: Point, mirror: string): Dir {
-  if (mirror === "/") {
-    switch (p.dir) {
-      case "right":
-        return "up";
-      case "left":
-        return "down";
-      case "up":
-        return "right";
-      case "down":
-        return "down";
-    }
-  }
-  if (mirror === "\\") {
-    switch (p.dir) {
-      case "right":
-        return "down";
-      case "down":
-        return "right";
-      case "left":
-        return "up";
-      case "up":
-        return "left";
-    }
-  }
+function hash(p: Point): string {
+  return p.x.toString() + "," + p.y.toString() + p.dir;
 }
 
-function outOfBounds(np: Point, gridraw: any[]) {
-  return (
-    np.x > gridraw[0].length || np.y > gridraw.length-1 || np.x < 0 || np.y < 0
-  );
+function rotate(dir: Dir, mirror: string): Dir {
+  const rot = new Map<Dir, Dir[]>([
+    ["R", ["U", "D"]],
+    ["L", ["D", "U"]],
+    ["U", ["R", "L"]],
+    ["D", ["L", "R"]],
+  ]);
+  return mirror === "/" ? rot.get(dir)[0] : rot.get(dir)[1];
+}
+
+function outOfBounds(p: Point, g: any[]) {
+  return p.x > g[0].length - 1 || p.y > g.length - 1 || p.x < 0 || p.y < 0;
 }
 
 function move(p: Point) {
   switch (p.dir) {
-    case "left":
+    case "L":
       p.x = p.x - 1;
       break;
-    case "right":
+    case "R":
       p.x = p.x + 1;
       break;
-    case "down":
+    case "D":
       p.y = p.y + 1;
       break;
-    case "up":
+    case "U":
       p.y = p.y - 1;
       break;
   }
   return p;
-}
-
-function solve2(lines: string[], sum = 0) {
-  return sum;
 }
